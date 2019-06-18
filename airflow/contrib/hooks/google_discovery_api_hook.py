2fba440a46cb0af2d34a2548b39487e09f1d2cef
Louis Guitton <louisguitton93@gmail.com>
2019-06-18 21:28:38 +0200

    use GoogleCloudBaseHook and rename to GoogleDiscoveryApiHook

--- airflow/contrib/hooks/google_discovery_api_hook.py
+++ airflow/contrib/hooks/google_discovery_api_hook.py
@@ -17,29 +17,27 @@
 # specific language governing permissions and limitations
 # under the License.
 #
-from google.oauth2.credentials import Credentials
 from googleapiclient.discovery import build
 
-from airflow.hooks.base_hook import BaseHook
+from airflow.contrib.hooks.gcp_api_base_hook import GoogleCloudBaseHook
 
 
-class GoogleApiClientHook(BaseHook):
+class GoogleDiscoveryApiHook(GoogleCloudBaseHook):
     """
     A hook to use the Google API Client library.
 
-    :param google_api_client_conn_id: Holds the connection information needed for
-        an google api client to be authenticated.
-    :type google_api_client_conn_id: str
     :param api_service_name: The name of the api service that is needed to get the data
         for example 'youtube'.
     :type api_service_name: str
     :param api_version: The version of the api that will be requested for example 'v3'.
     :type api_version: str
     """
+    _conn = None
 
-    def __init__(self, google_api_client_conn_id, api_service_name, api_version):
-        super(GoogleApiClientHook, self).__init__(google_api_client_conn_id)
-        self.google_api_client_conn_id = google_api_client_conn_id
+    def __init__(
+        self, api_service_name, api_version, gcp_conn_id='google_cloud_default', delegate_to=None
+    ):
+        super(GoogleDiscoveryApiHook, self).__init__(gcp_conn_id=gcp_conn_id, delegate_to=delegate_to)
         self.api_service_name = api_service_name
         self.api_version = api_version
 
@@ -52,16 +50,11 @@ class GoogleApiClientHook(BaseHook):
         """
         self.log.info("Authenticating Google API Client")
 
-        credentials = self._get_credentials_from_connection()
-
-        api_service = build(
-            serviceName=self.api_service_name,
-            version=self.api_version,
-            credentials=credentials,
-            cache_discovery=False
-        )
-
-        return api_service
+        if not self._conn:
+            http_authorized = self._authorize()
+            self._conn = build(serviceName=self.api_service_name, version=self.api_version,
+                               http=http_authorized, cache_discovery=False)
+        return self._conn
 
     def query(self, endpoint, data, paginate=False):
         """
@@ -83,23 +76,14 @@ class GoogleApiClientHook(BaseHook):
         """
         google_api_conn_client = self.get_conn()
 
-        api_response = self._call_api_request(google_api_conn_client, endpoint, data, paginate)
+        api_response = self._call_api_request(
+            google_api_conn_client, endpoint, data, paginate
+        )
         return api_response
 
-    def _get_credentials_from_connection(self):
-        conn = self.get_connection(self.google_api_client_conn_id)
-
-        info = {
-            'refresh_token': conn.schema,
-            'client_id': conn.login,
-            'client_secret': conn.password
-        }
-
-        scopes = conn.host.split(',')
-
-        return Credentials.from_authorized_user_info(info=info, scopes=scopes)
-
-    def _call_api_request(self, google_api_conn_client, endpoint, data, paginate):
+    def _call_api_request(
+        self, google_api_conn_client, endpoint, data, paginate
+    ):
         api_endpoint_parts = endpoint.split('.')
 
         google_api_endpoint_instance = self._build_api_request(
@@ -110,24 +94,32 @@ class GoogleApiClientHook(BaseHook):
 
         if paginate:
             return self._paginate_api(
-                google_api_endpoint_instance,
-                google_api_conn_client,
+                google_api_endpoint_instance, google_api_conn_client,
                 api_endpoint_parts
             )
 
         return google_api_endpoint_instance.execute()
 
-    def _build_api_request(self, google_api_conn_client, api_sub_functions, api_endpoint_params):
+    def _build_api_request(
+        self, google_api_conn_client, api_sub_functions, api_endpoint_params
+    ):
         for sub_function in api_sub_functions:
-            google_api_conn_client = getattr(google_api_conn_client, sub_function)
+            google_api_conn_client = getattr(
+                google_api_conn_client, sub_function
+            )
             if sub_function != api_sub_functions[-1]:
                 google_api_conn_client = google_api_conn_client()
             else:
-                google_api_conn_client = google_api_conn_client(**api_endpoint_params)
+                google_api_conn_client = google_api_conn_client(
+                    **api_endpoint_params
+                )
 
         return google_api_conn_client
 
-    def _paginate_api(self, google_api_endpoint_instance, google_api_conn_client, api_endpoint_parts):
+    def _paginate_api(
+        self, google_api_endpoint_instance, google_api_conn_client,
+        api_endpoint_parts
+    ):
         api_responses = []
 
         while google_api_endpoint_instance:
@@ -135,25 +127,28 @@ class GoogleApiClientHook(BaseHook):
             api_responses.append(api_response)
 
             google_api_endpoint_instance = self._build_next_api_request(
-                google_api_conn_client,
-                api_endpoint_parts[1:],
-                google_api_endpoint_instance,
-                api_response
+                google_api_conn_client, api_endpoint_parts[1:],
+                google_api_endpoint_instance, api_response
             )
 
         return api_responses
 
-    def _build_next_api_request(self,
-                                google_api_conn_client,
-                                api_sub_functions,
-                                api_endpoint_instance,
-                                api_response):
+    def _build_next_api_request(
+        self, google_api_conn_client, api_sub_functions, api_endpoint_instance,
+        api_response
+    ):
         for sub_function in api_sub_functions:
             if sub_function != api_sub_functions[-1]:
-                google_api_conn_client = getattr(google_api_conn_client, sub_function)
+                google_api_conn_client = getattr(
+                    google_api_conn_client, sub_function
+                )
                 google_api_conn_client = google_api_conn_client()
             else:
-                google_api_conn_client = getattr(google_api_conn_client, sub_function + '_next')
-                google_api_conn_client = google_api_conn_client(api_endpoint_instance, api_response)
+                google_api_conn_client = getattr(
+                    google_api_conn_client, sub_function + '_next'
+                )
+                google_api_conn_client = google_api_conn_client(
+                    api_endpoint_instance, api_response
+                )
 
         return google_api_conn_client
