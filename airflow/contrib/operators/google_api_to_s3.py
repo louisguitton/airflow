3f2f1bb0df9da5c3332627f2179761ea4657ac54
Louis Guitton <louisguitton93@gmail.com>
2019-06-20 22:10:58 +0200

    Address review comments

--- airflow/contrib/operators/google_api_to_s3.py
+++ airflow/contrib/operators/google_api_to_s3.py
@@ -17,9 +17,11 @@ class GoogleApiToS3Transfer(BaseOperator):
     :param google_api_service_version: The version of the API that is being requested.
     :type google_api_service_version: str
     :param google_api_endpoint_path: The client libraries path to the api call's executing method.
-                                     For example: 'analyticsreporting.reports.batchGet'
-                                     NOTE: See https://developers.google.com/apis-explorer
-                                     for more information on what methods are available.
+        For example: 'analyticsreporting.reports.batchGet'
+
+        .. note:: See https://developers.google.com/apis-explorer
+            for more information on what methods are available.
+
     :type google_api_endpoint_path: str
     :param google_api_endpoint_params: The params to control the corresponding endpoint result.
     :type google_api_endpoint_params: dict
@@ -27,23 +29,25 @@ class GoogleApiToS3Transfer(BaseOperator):
                                from the endpoint in S3.
     :type s3_destination_key: str
     :param google_api_response_via_xcom: Can be set to expose the google api response
-                                         to xcom.
-                                         The default value is None.
+        to xcom.
+        The default value is None.
     :type google_api_response_via_xcom: str
     :param google_api_endpoint_params_via_xcom: If set to a value this value will be used as a key
-                                                for pulling from xcom and updating the google api
-                                                endpoint params.
-                                                The default value is None.
+        for pulling from xcom and updating the google api
+        endpoint params.
+        The default value is None.
     :type google_api_endpoint_params_via_xcom: str
     :param google_api_endpoint_params_via_xcom_task_ids: Task ids to filter xcom by.
-                                                         The default value is None.
+        The default value is None.
     :type google_api_endpoint_params_via_xcom_task_ids: str or list of str
     :param google_api_pagination: If set to True Pagination will be enabled for this request
-                                  to retrieve all data.
-                                  NOTE: This means the response will be a list of responses.
+        to retrieve all data.
+
+        .. note:: This means the response will be a list of responses.
+
     :type google_api_pagination: bool
     :param s3_overwrite: Specifies whether the s3 file will be overwritten if exists.
-                         The default value is True.
+        The default value is True.
     :type s3_overwrite: bool
     :param gcp_conn_id: The connection ID to use when fetching connection info.
     :type gcp_conn_id: string
@@ -52,8 +56,8 @@ class GoogleApiToS3Transfer(BaseOperator):
         domain-wide delegation enabled.
     :type delegate_to: string
     :param s3_conn_id: The connection id specifying the authentication information
-                       for the S3 Bucket.
-                       The default value is 's3_default'.
+        for the S3 Bucket.
+        The default value is 's3_default'.
     :type s3_conn_id: str
     """
 
@@ -117,12 +121,12 @@ class GoogleApiToS3Transfer(BaseOperator):
 
         data = self._retrieve_data_from_google_api()
 
+        self._load_data_to_s3(data)
+
         if self.google_api_response_via_xcom:
             self._expose_google_api_response_via_xcom(
                 context['task_instance'], data
             )
-
-        self._load_data_to_s3(data)
 
     def _retrieve_data_from_google_api(self):
         google_api_client = GoogleDiscoveryApiHook(
@@ -131,10 +135,10 @@ class GoogleApiToS3Transfer(BaseOperator):
             api_service_name=self.google_api_service_name,
             api_version=self.google_api_service_version
         )
-        google_api_conn_client = google_api_client.get_conn()
-
-        google_api_response = self._call_google_api_request(
-            google_api_conn_client
+        google_api_response = google_api_client.query(
+            endpoint=self.google_api_endpoint_path,
+            data=self.google_api_endpoint_params,
+            paginate=self.google_api_pagination
         )
         return google_api_response
 
@@ -145,37 +149,6 @@ class GoogleApiToS3Transfer(BaseOperator):
             key=self.s3_destination_key,
             replace=self.s3_overwrite
         )
-
-    def _build_google_api_request(
-        self, google_api_conn_client, google_api_sub_functions
-    ):
-        for sub_function in google_api_sub_functions:
-            google_api_conn_client = getattr(
-                google_api_conn_client, sub_function
-            )
-            if sub_function != google_api_sub_functions[-1]:
-                google_api_conn_client = google_api_conn_client()
-            else:
-                google_api_conn_client = google_api_conn_client(
-                    **self.google_api_endpoint_params
-                )
-
-        return google_api_conn_client
-
-    def _call_google_api_request(self, google_api_conn_client):
-        google_api_endpoint_parts = self.google_api_endpoint_path.split('.')
-
-        google_api_endpoint_instance = self._build_google_api_request(
-            google_api_conn_client, google_api_endpoint_parts[1:]
-        )
-
-        if self.google_api_pagination:
-            return self._paginate_google_api(
-                google_api_endpoint_instance, google_api_conn_client,
-                google_api_endpoint_parts
-            )
-
-        return google_api_endpoint_instance.execute()
 
     def _update_google_api_endpoint_params_via_xcom(self, task_instance):
         google_api_endpoint_params = task_instance.xcom_pull(
@@ -188,41 +161,3 @@ class GoogleApiToS3Transfer(BaseOperator):
         task_instance.xcom_push(
             key=self.google_api_response_via_xcom, value=data
         )
-
-    def _paginate_google_api(
-        self, google_api_endpoint_instance, google_api_conn_client,
-        google_api_endpoint_parts
-    ):
-        google_api_responses = []
-
-        while google_api_endpoint_instance:
-            google_api_response = google_api_endpoint_instance.execute()
-
-            google_api_responses.append(google_api_response)
-
-            google_api_endpoint_instance = self._build_next_google_api_request(
-                google_api_conn_client, google_api_endpoint_parts[1:],
-                google_api_endpoint_instance, google_api_response
-            )
-
-        return google_api_responses
-
-    def _build_next_google_api_request(
-        self, google_api_conn_client, google_api_sub_functions,
-        google_api_endpoint_instance, google_api_response
-    ):
-        for sub_function in google_api_sub_functions:
-            if sub_function != google_api_sub_functions[-1]:
-                google_api_conn_client = getattr(
-                    google_api_conn_client, sub_function
-                )
-                google_api_conn_client = google_api_conn_client()
-            else:
-                google_api_conn_client = getattr(
-                    google_api_conn_client, sub_function + '_next'
-                )
-                google_api_conn_client = google_api_conn_client(
-                    google_api_endpoint_instance, google_api_response
-                )
-
-        return google_api_conn_client
